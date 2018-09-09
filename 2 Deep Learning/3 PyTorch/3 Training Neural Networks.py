1132160d394198583defdd18bd9decc113805080
ciancronin <42519113+ciancronin@users.noreply.github.com>
2018-09-09 19:15:49 +0100

    PyTorch Training and Validation

--- 2 Deep Learning/3 PyTorch/3 Training Neural Networks.py
+++ 2 Deep Learning/3 PyTorch/3 Training Neural Networks.py
@@ -1,5 +1,144 @@
 #
 # @author - Cian Cronin (croninc@google.com)
 # @description - 3 Training Neural Networks
-# @date - 03/09/2018
+# @date - 09/09/2018
 #
+
+# Torch provides the module autograd for automatically calculating the
+# gradient of tensors
+
+from collections import OrderedDict
+
+import numpy as np
+import time
+
+import torch
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+
+# You can turn off gradients for a block of code with the `torch.no_grad()`
+# content:
+x = torch.zeros(1, requires_grad=True)
+with torch.no_grad():
+    y = x * 2
+y.requires_grad
+# returns - False
+
+x = torch.randn(2,2, requires_grad=True)
+print(x)
+
+y = x**2
+print(y)
+
+## grad_fn shows the function that generated this variable
+print(y.grad_fn)
+
+z = y.mean()
+print(z)
+
+print(x.grad)
+
+# To calculate the gradients, you need to run the `.backward` method on a
+# Variable, `z` for example. This will calculate the gradient for `z` with
+# respect to `x`
+
+z.backward()
+print(x.grad)
+print(x/2)
+
+# Get the data and define the network
+from torchvision import datasets, transforms
+
+# Define a transform to normalize the data
+transform = transforms.Compose([transforms.ToTensor(),
+                              transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                             ])
+# Download and load the training data
+trainset = datasets.MNIST('MNIST_data/', download=True, train=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+
+# Hyperparameters for our network
+input_size = 784
+hidden_sizes = [128, 64]
+output_size = 10
+
+# Build a feed-forward network
+model = nn.Sequential(OrderedDict([
+                      ('fc1', nn.Linear(input_size, hidden_sizes[0])),
+                      ('relu1', nn.ReLU()),
+                      ('fc2', nn.Linear(hidden_sizes[0], hidden_sizes[1])),
+                      ('relu2', nn.ReLU()),
+                      ('logits', nn.Linear(hidden_sizes[1], output_size))]))
+
+# Training the network!
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+
+# First, let's consider just one learning step before looping through all the
+# data. The general process with PyTorch:
+#
+# * Make a forward pass through the network to get the logits
+# * Use the logits to calculate the loss
+# * Perform a backward pass through the network with `loss.backward()` to
+#       calculate the gradients
+# * Take a step with the optimizer to update the weights
+#
+# Below I'll go through one training step and print out the weights and
+# gradients so you can see how it changes.
+print('Initial weights - ', model.fc1.weight)
+
+images, labels = next(iter(trainloader))
+images.resize_(64, 784)
+
+# Clear the gradients, do this because gradients are accumulated
+optimizer.zero_grad()
+
+# Forward pass, then backward pass, then update weights
+output = model.forward(images)
+loss = criterion(output, labels)
+loss.backward()
+print('Gradient -', model.fc1.weight.grad)
+optimizer.step()
+
+print('Updated weights - ', model.fc1.weight)
+
+# Training for real
+optimizer = optim.SGD(model.parameters(), lr=0.003)
+epochs = 3
+print_every = 40
+steps = 0
+for e in range(epochs):
+    running_loss = 0
+    for images, labels in iter(trainloader):
+        steps += 1
+        # Flatten MNIST images into a 784 long vector
+        images.resize_(images.size()[0], 784)
+
+        optimizer.zero_grad()
+
+        # Forward and backward passes
+        output = model.forward(images)
+        loss = criterion(output, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+        if steps % print_every == 0:
+            print("Epoch: {}/{}... ".format(e+1, epochs),
+                  "Loss: {:.4f}".format(running_loss/print_every))
+
+            running_loss = 0
+
+# We can then check predicions via
+images, labels = next(iter(trainloader))
+
+img = images[0].view(1, 784)
+# Turn off gradients to speed up this part
+with torch.no_grad():
+    logits = model.forward(img)
+
+# Output of the network are logits, need to take softmax for probabilities
+ps = F.softmax(logits, dim=1)
+helper.view_classify(img.view(1, 28, 28), ps)
---
+++ 2 Deep Learning/3 PyTorch/4 Fashion MNIST Exercise.py
@@ -0,0 +1,90 @@
+#
+# @author - Cian Cronin (croninc@google.com)
+# @description - 4 Fashion MNIST Exercise
+# @date - 09/09/2018
+#
+
+import torch
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+
+# Define a transform to normalize the data
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+# Download and load the training data
+trainset = datasets.FashionMNIST('F_MNIST_data/', download=True, train=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+
+# Download and load the test data
+testset = datasets.FashionMNIST('F_MNIST_data/', download=True, train=False, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=True)
+
+image, label = next(iter(trainloader))
+
+# TODO: Define your network architecture here
+input_size = 784
+hidden_sizes = [128, 64]
+output_size = 10
+
+model = nn.Sequential(nn.Linear(input_size, hidden_sizes[0]),
+                      nn.ReLU(),
+                      nn.Linear(hidden_sizes[0], hidden_sizes[1]),
+                      nn.ReLU(),
+                      nn.Linear(hidden_sizes[1], output_size),
+                      nn.Softmax(dim=1))
+print(model)
+
+# TODO: Create the network, define the criterion and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+
+# TODO: Train the network here
+epochs = 20
+print_every = 100
+steps = 0
+for e in range(epochs):
+    running_loss = 0
+    for images, labels in iter(trainloader):
+        steps += 1
+        # Flatten MNIST images into a 784 long vector
+        images.resize_(images.size()[0], 784)
+
+        optimizer.zero_grad()
+
+        # Forward and backward passes
+        output = model.forward(images)
+        loss = criterion(output, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+        if steps % print_every == 0:
+            print("Epoch: {}/{}... ".format(e+1, epochs),
+                  "Loss: {:.4f}".format(running_loss/print_every))
+
+            running_loss = 0
+
+# Test out your network!
+
+dataiter = iter(testloader)
+images, labels = dataiter.next()
+img = images[0]
+# Convert 2D image to 1D vector
+img = img.resize_(1, 784)
+
+# TODO: Calculate the class probabilities (softmax) for img
+with torch.no_grad():
+    logits = model.forward(img)
+
+# Output of the network are logits, need to take softmax for probabilities
+ps = F.softmax(logits, dim=1)
+
+# Plot the image and probabilities
+# helper.view_classify(img.resize_(1, 28, 28), ps, version='Fashion')
